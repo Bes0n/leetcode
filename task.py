47f253ee9b6db99e92ee43d418538d9c96526b1b
Ilgar <barbiga.diamond@gmail.com>
2023-07-31 16:37:07 +0200

    started hashing

--- task.py
+++ task.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3.10
 
-nums = [1, 6, 3, 2, 7, 2]
-queries = [[0, 3], [2, 5], [2, 4]]
-limit = 13
+# nums = [7,4,3,9,1,8,5,2,6] 
+nums = [5, 2, 7, 10, 3, 9]
+target = 8
 
-def answer_queries(nums, queries, limit):
-    prefix = [nums[0]]
-    for i in range(1, len(nums)):
-        # print(prefix[-1])
-        # print(nums[i])
-        prefix.append(nums[i] + prefix[-1])
-    ans = []
-    for x, y in queries:
-        curr = prefix[y] - prefix[x] + nums[x]
-        print(curr)
-        ans.append(curr < limit)
+def getAverages(nums, target):
+    dic = {}
+    for i in range(len(nums)):
+        num = nums[i]
+        complement = target - num
+        if complement in dic: # This operation is O(1)!
+            return [nums[i], complement]
+        
+        dic[num] = i
+    
+    return [-1, -1]
 
-    # return ans
-
-# [1, 7, 10, 12, 19, 21]
-# x = 1 10 10
-# y = 12 21 19 
-
-print(answer_queries(nums, queries, limit))
+print(getAverages(nums, target))
