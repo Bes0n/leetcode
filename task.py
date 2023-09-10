390170ff56a35c53b704808d48d3e1d59fd37d11
BesOn <barbiga.diamond@gmail.com>
2023-09-10 15:21:37 +0200

    more hash maps

--- task.py
+++ task.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3.10
 import collections
 
-ransomNote = "aab"
-magazine = "aabb"
+jewels = "aA"
+stones = "aAAbbbb"
 
-def canConstruct(ransomNote, magazine):
-    # Check for obvious fail case.
-    if len(ransomNote) > len(magazine): return False
+def numJewelsInStones(jewels, stones):
+    j = collections.Counter(jewels)
+    s = collections.Counter(stones)
 
-    # In Python, we can use the Counter class. It does all the work that the
-    # makeCountsMap(...) function in our pseudocode did!
-    magazine_counts = collections.Counter(magazine)
-    ransom_note_counts = collections.Counter(ransomNote)
+    print(j)
+    print(s)
+    
 
-    # For each *unique* character in the ransom note:
-    for char, count in ransom_note_counts.items():
-        # Check that the count of char in the magazine is equal
-        # or higher than the count in the ransom note.
-        magazine_count = magazine_counts[char]
-        if magazine_count < count:
-            return False
-            
-    # # If we got this far, we can successfully build the note.
-    # return True
-
-print(canConstruct(ransomNote, magazine))
+print(numJewelsInStones(jewels, stones))
