b646547e4e387bf164642861ed794e0a878dd5b7
BesOn <barbiga.diamond@gmail.com>
2023-09-11 11:27:28 +0200

    introduction to classes and objects

--- task.py
+++ task.py
@@ -1,46 +1,11 @@
 #!/usr/bin/python3.10
 
-class House():
-    # House description
-    def __init__(self, street, number) -> None:
-        self.street = street
-        self.number = number
-        self.age = 0
-    
-    def build(self):
-        # Building the house
-        print("House number {} on street {} has been built".format(self.number, self.street))
-    
-    def ageofhouse(self, year):
-        # Age of House
-        self.age += year
+class ListNode():
+    def __init__(self, val) -> None:
+        self.val = val
+        self.next = None
 
 
-House1 = House("Zizkov", 35)
-House2 = House("Vinohrady", 10)
+one = ListNode(1)
 
-# Calling object, Output: Zizkov
-print(House1.street)
-
-# Calling object, Output: 10
-print(House2.number)
-
-# Calling method, Output: House number 35 on street Zizkov has been built
-print(House1.build())
-
-# Calling method and updating age of the house
-House1.ageofhouse(5)
-print(House1.age) # Output 5
-print(House2.age) # Output 0
-
-
-# Inheritance
-class AvenueHouse(House):
-    def __init__(self, avenue, number) -> None:
-        super().__init__(self, number)
-        self.avenue = avenue
-        self.number = number
-
-AveHouse = AvenueHouse("Legerova", 25)
-
-print(AveHouse.avenue)
+print(one)
